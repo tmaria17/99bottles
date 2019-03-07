7564f5e4a3db30cfaa794f5ad34f259436cc330c
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-03-06 22:33:33 -0700

    Shameless green up until test_verse_0.

---
+++ lib/bottles.rb
@@ -0,0 +1,27 @@
+class Bottles
+
+  def verse(number)
+    lower_num = number -1
+  if number == 0
+    <<-VERSE
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+    VERSE
+  elsif lower_num > 1
+    <<-VERSE
+#{number} bottles of beer on the wall, #{number} bottles of beer.
+Take one down and pass it around, #{lower_num} bottles of beer on the wall.
+VERSE
+  elsif lower_num == 1
+    <<-VERSE
+#{number} bottles of beer on the wall, #{number} bottles of beer.
+Take one down and pass it around, #{lower_num} bottle of beer on the wall.
+VERSE
+  elsif lower_num == 0
+    <<-VERSE
+1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+    VERSE
+  end
+  end
+end
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -1,6 +1,7 @@
 gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require_relative '../lib/bottles'
+require 'pry'
 
 class BottlesTest < Minitest::Test
   def test_the_first_verse
@@ -8,11 +9,12 @@ class BottlesTest < Minitest::Test
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 VERSE
+# binding.pry
     assert_equal expected, ::Bottles.new.verse(99)
   end
 
   def test_another_verse
-    skip
+
     expected = <<-VERSE
 89 bottles of beer on the wall, 89 bottles of beer.
 Take one down and pass it around, 88 bottles of beer on the wall.
@@ -21,7 +23,7 @@ VERSE
   end
 
   def test_verse_2
-    skip
+
     expected = <<-VERSE
 2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
@@ -30,7 +32,6 @@ Take one down and pass it around, 1 bottle of beer on the wall.
   end
 
   def test_verse_1
-    skip
     expected = <<-VERSE
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
@@ -39,7 +40,6 @@ Take it down and pass it around, no more bottles of beer on the wall.
   end
 
   def test_verse_0
-    skip
     expected = <<-VERSE
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
